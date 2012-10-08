d99b26379d2b24b92e21137a48cc4d49a5f7094a
Luke Miles <luke@lukemil.es>
2012-10-07 20:36:42 -0700

    initial commit with random picker and examples

---
+++ random.php
@@ -0,0 +1,18 @@
+<?
+function randomLyric () {
+	//create an array of all of the files in the directory
+	$songs = glob("/home/ubuntu/twitterbot/songs/*.txt");
+
+	//picks a random file, uses it as the song
+	$song = $songs[array_rand($songs, 1)];
+
+	//loads the content of the lyric file
+	$f_contents = file($song);
+
+	//picks a random line
+	$line = $f_contents[array_rand($f_contents)];
+
+	return $line;
+}
+echo randomLyric ();
+?>
